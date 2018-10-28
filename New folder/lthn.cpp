ce505dc4abacb80cac2422ec5fd0daa02995272d
arjuna14 <arjuna@mhs.pelitabangsa.ac.id>
2018-10-28 09:17:30 +0700

    lthn

---
+++ New folder/lthn.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main ()
+{
+    int i = 0;
+    int max=0;
+    int n,a;
+    cout << "masukan jumlah bilagan :" ;
+    cin >> n;
+
+    for (i;i<n;i++) {
+        cout << "masukan bilangan ke-" << i+1 << ":" ;
+        cin >> a;
+
+        if (max<a)
+            max = a;
+
+    }
+    cout << "bilangan terbesar adalah: " << max << endl;
+}
